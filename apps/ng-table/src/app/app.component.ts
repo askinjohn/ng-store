ecb69b10ede40d71e7e682716c9e96b27364f695
Askin John Samuel <sam@MINAJ201.local>
2023-08-02 22:47:15 +0530

    Initial commit

---
+++ apps/ng-table/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-ng-store-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng-table';
+}
