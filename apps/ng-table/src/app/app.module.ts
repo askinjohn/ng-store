275e2d7f764465da138e9a9b87f600a7fc4c53ea
Askin John Samuel <sam@MINAJ201.local>
2023-08-04 00:08:34 +0530

    feat(ng-table-ui): added basic css and structure

--- apps/ng-table/src/app/app.module.ts
+++ apps/ng-table/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { TableComponent } from 'libs/ng-table-ui/src/lib/table.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, TableComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
