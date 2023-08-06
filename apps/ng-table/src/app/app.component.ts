8d719890651326f3b0c9f027303bc162d7217ae1
Askin John Samuel <sam@MINAJ201.local>
2023-08-06 15:44:28 +0530

    feat(ngtable): basic structuring design and logic

--- apps/ng-table/src/app/app.component.ts
+++ apps/ng-table/src/app/app.component.ts
@@ -7,4 +7,81 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-table';
+
+  data: any = {
+    headers: [
+      'Merchant Info',
+      'MerchantId',
+      'No. of Transactions',
+      'Total Amount',
+      'Latest Activity',
+    ],
+    values: [
+      {
+        merchantName: 'Askin',
+        companyId: '2313212',
+        email: 'as@sd.se',
+        merchantId: '8432432224',
+        acquirerId: 'ppfn93hh',
+        totalNumberOfTransaction: '50',
+        totalAmountOfTransaction: '502342',
+        lastTransactionAt: '2023 4 April ',
+      },
+      {
+        merchantName: 'Askin',
+        companyId: '2313212',
+        email: 'as@sd.se',
+        merchantId: '8432432224',
+        acquirerId: 'ppfn93hh',
+        totalNumberOfTransaction: '50',
+        totalAmountOfTransaction: '502342',
+        lastTransactionAt: '2023 4 April ',
+      },
+      {
+        merchantName: 'Askin',
+        companyId: '2313212',
+        email: 'as@sd.se',
+        merchantId: '8432432224',
+        acquirerId: 'ppfn93hh',
+        totalNumberOfTransaction: '50',
+        totalAmountOfTransaction: '502342',
+        lastTransactionAt: '2023 4 April ',
+      },
+      {
+        merchantName: 'Askin',
+        companyId: '2313212',
+        email: 'as@sd.se',
+        merchantId: '8432432224',
+        acquirerId: 'ppfn93hh',
+        totalNumberOfTransaction: '50',
+        totalAmountOfTransaction: '502342',
+        lastTransactionAt: '2023 4 April ',
+      },
+
+      {
+        merchantName: 'Askin',
+        companyId: '2313212',
+        email: 'as@sd.se',
+        merchantId: '8432432224',
+        acquirerId: 'ppfn93hh',
+        totalNumberOfTransaction: '50',
+        totalAmountOfTransaction: '502342',
+        lastTransactionAt: '2023 4 April ',
+      },
+    ],
+    props: [
+      [
+        { header: '', key: 'merchantName' },
+        { header: 'Org No:', key: 'companyId' },
+        { header: '', key: 'email' },
+      ],
+      [
+        { header: '', key: 'merchantId' },
+        { header: '', key: 'acquirerMID' },
+      ],
+      [{ header: '', key: 'totalNumberOfTransaction' }],
+      [{ header: '', key: 'totalAmountOfTransaction' }],
+      [{ header: 'Activity:', key: 'lastTransactionAt' }],
+    ],
+  };
 }
--- libs/ng-table-ui/src/lib/table.component.ts
+++ libs/ng-table-ui/src/lib/table.component.ts
@@ -1,20 +1,31 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ng-table-ui',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   template: `<div class="w-full bg-blue-500 p-2 text-blue-400">
     <table>
       <thead>
         <tr>
-          <th *ngFor="let header of tableHeaders">{{ header }}</th>
+          <th class="theader-text" *ngFor="let header of inputData.headers">
+            {{ header }}
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td *ngFor="let data of tableData">{{ data }}</td>
+        <tr *ngFor="let value of inputData.values">
+          <td class="tval-text" *ngFor="let prop of inputData.props">
+            <ng-container *ngFor="let p of prop">
+              <span class="row-item ">
+                <p *ngIf="value[p['key']]" class="row-header">
+                  {{ p['header'] }}
+                </p>
+                <p class="row-value">{{ value[p['key']] }}</p>
+              </span>
+            </ng-container>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -22,6 +33,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./table.styles.scss'],
 })
 export class TableComponent {
-  tableHeaders = ['Title', 'Title', 'Title', 'Title', 'Title'];
-  tableData = ['one', 'two', 'three', 'four', 'five'];
+  clickable = false;
+  @Input() inputData!: IInput;
 }
+
+export type IInput = {
+  headers: Array<string>;
+  values: Record<string, string | number | boolean>[];
+  props: Array<
+    Array<{
+      key: string;
+      header: string;
+      type?: string;
+    }>
+  >;
+};
